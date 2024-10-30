b0fee55475cc2836a7d2a6b0c64f6c3069e25908
Panos ツ <121546982+Panos0210@users.noreply.github.com>
2024-10-30 18:23:02 +0200

    Add files via upload

---
+++ All Registry Tweaks.bat
@@ -0,0 +1,194 @@
+@echo off
+echo Installing All Reg Tweaks...
+
+echo Minimal Processes...
+reg add "HKLM\SYSTEM\CurrentControlSet\Control" /v "SvcHostSplitThresholdInKB" /t REG_DWORD /d "4000000" /f
+ 
+
+echo Disabling Automatic App Instalation...
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "SilentInstalledAppsEnabled" /t REG_DWORD /d "0" /f
+
+echo Enabling Starting File Manager on This PC...
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "LaunchTo" /t REG_DWORD /d "1" /f
+
+echo Show Hidden Files...
+reg add "HKCU\Software\Policies\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Hidden" /t REG_DWORD /d "1" /f
+
+echo Disabling Copilot...
+reg add "HKCU\Software\Policies\Microsoft\Windows\WindowsCopilot" /v "TurnOffWindowsCopilot" /t REG_DWORD /d "1" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowCopilotButton" /t REG_DWORD /d "0" /f
+ 
+
+echo Disabling Shortcut Arrow...
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Icons" /v "29" /t REG_SZ /d " " /f
+
+echo Disabling "- Shortcut" on Shortcuts...
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v "link" /t REG_BINARY /d "00000000" /f
+ 
+
+echo Disabling Web Search...
+reg add "HKCU\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v "DisableSearchBoxSuggestions" /t REG_DWORD /d "1" /f
+ 
+
+echo Disabling Cortana...
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /V "AllowCortana" /t REG_DWORD /d "0" /f
+
+echo Show File Extensions...
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideFileExt" /t REG_DWORD /d "0" /f
+
+
+echo Disabling Immersive Context Menu...
+reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+
+
+echo Disabling Taksbar Widgets...
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Dsh" /v "AllowNewsAndInterests" /t REG_DWORD /d "0" /f
+
+
+echo Enabling Hibrenation...
+   powercfg.exe /hibernate on
+
+
+echo Enabling Ultimate Performace Powerplan...
+reg add "HKLM\System\CurrentControlSet\Control\Power" /v "PlatformAoAcOverride" /t REG_DWORD /d "0" /f
+   powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
+
+
+echo Disabling Power Throttling...
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerThrottling" /v "PowerThrottlingOff" /t REG_DWORD /d "1" /f
+
+
+echo Disabling OneDrive...
+reg add "HKCU\Software\Microsoft\OneDrive" /v "DisableFileSyncNGSC" /t REG_DWORD /d "1"
+
+
+echo Enable Only Security Updates...
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "ProductVersion" /t REG_SZ /d "Windows 11" /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "TargetReleaseVersion" /t REG_DWORD /d "1" /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "TargetReleaseVersionInfo" /t REG_SZ /d "23H2" /f
+
+
+echo Disable Logon Blur...
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "DisableAcrylicBackgroundOnLogon" /t REG_DWORD /d "1" /f
+
+
+echo Disable BSOD Smiley and Enable Details...
+reg add "HKLM\System\CurrentControlSet\Control\CrashControl" /v "DisplayParameters" /t REG_DWORD /d "1" /f 
+reg add "HKLM\System\CurrentControlSet\Control\CrashControl" /v "CrashControl" /t REG_DWORD /d "1" /f 
+
+
+echo Disabling Wallpaper Quality Reduction...
+reg add "HKCU\Control Panel\Desktop" /v "JPEGImportQuality" /t REG_DWORD /d "100" /f
+
+
+echo Disabling Mouse Acceleration... 
+reg add "HKCU\Control Panel\Mouse" /v "MouseSpeed" /t REG_SZ /d "0" /f
+reg add "HKCU\Control Panel\Mouse" /v "MouseThreshold1" /t REG_SZ /d "0" /f
+reg add "HKCU\Control Panel\Mouse" /v "MouseThreshold2" /t REG_SZ /d "0" /f
+
+
+echo Disabling Telemetry...
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v "AllowTelemetry" /t REG_DWORD /d "0" /f
+
+
+echo Removing Edge...
+@echo off
+
+:: Creator: Dave Kirkwood
+:: Modified:By Britec
+:: Created: 24/09/2020
+:: Updated: 21/09/2022
+::
+
+:: First Stop Microsoft Edge Task
+taskkill /F /IM msedge.exe  >nul 2>&1
+
+
+CD %HOMEDRIVE%%HOMEPATH%\Desktop
+echo %CD%
+
+
+REM ************ Main process *****************
+
+echo *** Removing Microsoft Edge ***
+call :killdir C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe
+call :killdir "C:\Program Files (x86)\Microsoft\Edge"
+call :killdir "C:\Program Files (x86)\Microsoft\EdgeUpdate"
+call :killdir "C:\Program Files (x86)\Microsoft\EdgeCore"
+call :killdir "C:\Program Files (x86)\Microsoft\EdgeWebView"
+echo *** Modifying registry ***
+call :editreg
+echo *** Removing shortcuts ***
+call :delshortcut "C:\Users\Public\Desktop\Microsoft Edge.lnk"
+call :delshortcut "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Microsoft Edge.lnk"
+call :delshortcut "%APPDATA%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Microsoft Edge.lnk"
+echo Finished!
+pause
+exit
+
+REM ************ KillDir: Take ownership and remove a directory *****************
+
+:killdir
+echo|set /p=Removing dir %1
+if exist %1 (
+	takeown /a /r /d Y /f %1 > NUL
+	icacls %1 /grant administrators:f /t > NUL
+	rd /s /q %1 > NUL
+	if exist %1 (
+		..Failed.
+	) else (
+		..Deleted.
+	)
+) else (
+	..does not exist.
+)
+exit /B 0
+
+REM ************ Edit registry to add do not update Edge key *****************
+
+:editreg
+echo|set /p=Editting registry
+echo Windows Registry Editor Version 5.00 > RemoveEdge.reg
+echo. >> RemoveEdge.reg
+echo [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\EdgeUpdate] >> RemoveEdge.reg
+echo "DoNotUpdateToEdgeWithChromium"=dword:00000001 >> RemoveEdge.reg
+echo. >> RemoveEdge.reg
+echo [-HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{9459C573-B17A-45AE-9F64-1857B5D58CEE}] >> RemoveEdge.reg
+
+regedit /s RemoveEdge.reg
+del RemoveEdge.reg
+..done.
+exit /B 0
+
+:: Delete Microsoft Edge Folders
+rmdir /s /q "C:\Program Files (x86)\Microsoft\Edge"
+rmdir /s /q "C:\Program Files (x86)\Microsoft\EdgeCore"
+rmdir /s /q "C:\Program Files (x86)\Microsoft\EdgeWebView"
+rmdir /s /q "C:\Program Files (x86)\Microsoft\Temp"
+
+:: Delete Edge Icons, from all users
+for /f "delims=" %%a in ('dir /b "C:\Users"') do (
+del /S /Q "C:\Users\%%a\Desktop\edge.lnk" >nul 2>&1
+del /S /Q "C:\Users\%%a\Desktop\Microsoft Edge.lnk" >nul 2>&1)
+
+:: Delete additional files
+if exist "C:\Windows\System32\MicrosoftEdgeCP.exe" (
+for /f "delims=" %%a in ('dir /b "C:\Windows\System32\MicrosoftEdge*"') do (
+takeown /f "C:\Windows\System32\%%a" > NUL 2>&1
+icacls "C:\Windows\System32\%%a" /inheritance:e /grant "%UserName%:(OI)(CI)F" /T /C > NUL 2>&1
+del /S /Q "C:\Windows\System32\%%a" > NUL 2>&1))
+
+:delshortcut
+echo|set /p=Removing shortcut %1
+if exist %1 (
+	del %1
+	..done.
+) else (
+	..does not exist.
+)
+exit /B 0
+
+
+pause
+shutdown /r /t 0
+
