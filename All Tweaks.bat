2059b7393420b015265f64d51ff69500da7ee511
Panos ツ <121546982+Panos0210@users.noreply.github.com>
2024-11-27 15:06:40 +0200

    Update All Tweaks.bat

--- All Tweaks.bat
+++ All Tweaks.bat
@@ -13,168 +13,87 @@ rm winget.msixbundle
 echo Disabling Automatic App Instalation...
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "SilentInstalledAppsEnabled" /t REG_DWORD /d "0" /f
 
-echo ‎ 
-echo ‎ 
-
 echo Enabling Starting File Manager on This PC...
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "LaunchTo" /t REG_DWORD /d "1" /f
 
-echo ‎ 
-echo ‎
-
 echo Show Hidden Files...
 reg add "HKCU\Software\Policies\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Hidden" /t REG_DWORD /d "1" /f
-
-echo ‎ 
-echo ‎
 
 echo Disabling Copilot...
 reg add "HKCU\Software\Policies\Microsoft\Windows\WindowsCopilot" /v "TurnOffWindowsCopilot" /t REG_DWORD /d "1" /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowCopilotButton" /t REG_DWORD /d "0" /f
 
-echo ‎ 
-echo ‎
- 
 echo Disabling Shortcut Arrow...
 reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Icons" /v "29" /t REG_SZ /d " " /f
-
-echo ‎ 
-echo ‎
 
 echo Disabling "- Shortcut" on Shortcuts...
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v "link" /t REG_BINARY /d "00000000" /f
 
-echo ‎ 
-echo ‎
-
 echo Disabling Cortana...
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /V "AllowCortana" /t REG_DWORD /d "0" /f
-
-echo ‎ 
-echo ‎
 
 echo Show File Extensions...
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideFileExt" /t REG_DWORD /d "0" /f
 
-echo ‎ 
-echo ‎
-
-
 echo Disabling Immersive Context Menu...
 reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
-
-echo ‎ 
-echo ‎
-
 
 echo Disabling Taksbar Widgets...
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Dsh" /v "AllowNewsAndInterests" /t REG_DWORD /d "0" /f
 
-echo ‎ 
-echo ‎
-
-
 echo Enabling Hibrenation...
    powercfg.exe /hibernate on
-
-echo ‎ 
-echo ‎
-
 
 echo Enabling Ultimate Performace Powerplan...
 reg add "HKLM\System\CurrentControlSet\Control\Power" /v "PlatformAoAcOverride" /t REG_DWORD /d "0" /f
    powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
 
-echo ‎ 
-echo ‎
-
-
 echo Disabling Power Throttling...
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerThrottling" /v "PowerThrottlingOff" /t REG_DWORD /d "1" /f
 
-echo ‎ 
-echo ‎
-
-
 echo Disabling OneDrive...
 reg add "HKCU\Software\Microsoft\OneDrive" /v "DisableFileSyncNGSC" /t REG_DWORD /d "1" /f
-
-echo ‎ 
-echo ‎
-
 
 echo Enable Only Security Updates...
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "ProductVersion" /t REG_SZ /d "Windows 11" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "TargetReleaseVersion" /t REG_DWORD /d "1" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "TargetReleaseVersionInfo" /t REG_SZ /d "23H2" /f
 
-echo ‎ 
-echo ‎
-
-
 echo Disable Logon Blur...
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "DisableAcrylicBackgroundOnLogon" /t REG_DWORD /d "1" /f
-
-echo ‎ 
-echo ‎
-
 
 echo Disable BSOD Smiley and Enable Details...
 reg add "HKLM\System\CurrentControlSet\Control\CrashControl" /v "DisplayParameters" /t REG_DWORD /d "1" /f 
 reg add "HKLM\System\CurrentControlSet\Control\CrashControl" /v "CrashControl" /t REG_DWORD /d "1" /f 
 
-echo ‎ 
-echo ‎
-
 echo Disabling Wallpaper Quality Reduction...
 reg add "HKCU\Control Panel\Desktop" /v "JPEGImportQuality" /t REG_DWORD /d "100" /f
-
-echo ‎ 
-echo ‎
 
 echo Disabling Mouse Acceleration... 
 reg add "HKCU\Control Panel\Mouse" /v "MouseSpeed" /t REG_SZ /d "0" /f
 reg add "HKCU\Control Panel\Mouse" /v "MouseThreshold1" /t REG_SZ /d "0" /f
 reg add "HKCU\Control Panel\Mouse" /v "MouseThreshold2" /t REG_SZ /d "0" /f
 
-echo ‎ 
-echo ‎
-
 echo Disabling Telemetry...
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v "AllowTelemetry" /t REG_DWORD /d "0" /f
-
-echo ‎ 
-echo ‎
 
 echo Disabling the Weather/News icon in the Taskbar...
 reg add "HkLM\SOFTWARE\Policies\Microsoft\Dsh" /v "AllowNewsAndInterests" /t REG_DWORD /d "0" /f 
 
-echo ‎ 
-echo ‎
-
 echo Enabling Verbose...
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "verbosestatus" /t REG_DWORD /d "1" /f
-
-echo ‎ 
-echo ‎
 
 echo Reducing Resource Allocation to Background Tasks...
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /v "SystemResponsiveness" /t REG_DWORD /d "10" /f
 
-echo ‎ 
-echo ‎
-
 echo Hastening the Shutdown Process...
 reg add "HKLM\SYSTEM\CurrentControlSet\Control" /v "WaitToKillServiceTimeout" /t REG_SZ /d "500" /f
-
-echo ‎ 
-echo ‎
 
 echo Geting Rid of Bing Suggestions from the Start Menu...
 reg add "HKCU\Software\Policies\Microsoft\Windows\Explorer" /v "DisableSearchBoxSuggestions" /t REG_DWORD /d "1" /f
 
-echo ‎ 
-echo ‎
+
+
 
 pause
 echo Now it is going to uninstall Microsoft Edge to continue press any key to leave close this window
