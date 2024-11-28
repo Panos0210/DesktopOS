b7464e4e77c437a9f1db794f2311c3212d9c9af0
Panos ツ <121546982+Panos0210@users.noreply.github.com>
2024-11-28 15:24:39 +0200

    Create Enable Only Security Updates.bat

---
+++ Enable Only Security Updates.bat
@@ -0,0 +1,6 @@
+@echo off 
+echo Enable Only Security Updates...
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "ProductVersion" /t REG_SZ /d "Windows 11" /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "TargetReleaseVersion" /t REG_DWORD /d "1" /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "TargetReleaseVersionInfo" /t REG_SZ /d "23H2" /f
+pause
