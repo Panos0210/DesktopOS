261f2f4d57822d9ce7059b20d5c5437154f88c7c
panso <121546982+Panos0210@users.noreply.github.com>
2024-12-10 19:39:35 +0200

    Update All Tweaks.bat

--- All Tweaks.bat
+++ All Tweaks.bat
@@ -5,33 +5,53 @@ echo Installing All Tweaks...
 echo Installing Winget...
 powershell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile winget.msixbundle; Add-AppxPackage .\winget.msixbundle; Remove-Item -Force .\winget.msixbundle"
 
+echo.
+
 echo Disabling Automatic App Instalation...
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "SilentInstalledAppsEnabled" /t REG_DWORD /d "0" /f
+
+echo.
 
 echo Enabling Starting File Manager on This PC...
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "LaunchTo" /t REG_DWORD /d "1" /f
 
+echo.
+
 echo Show Hidden Files...
 reg add "HKCU\Software\Policies\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Hidden" /t REG_DWORD /d "1" /f
+
+echo.
 
 echo Disabling Copilot...
 reg add "HKCU\Software\Policies\Microsoft\Windows\WindowsCopilot" /v "TurnOffWindowsCopilot" /t REG_DWORD /d "1" /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowCopilotButton" /t REG_DWORD /d "0" /f
 
+echo.
+
 echo Disabling Shortcut Arrow...
 reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Icons" /v "29" /t REG_SZ /d " " /f
+
+echo.
 
 echo Disabling "- Shortcut" on Shortcuts...
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v "link" /t REG_BINARY /d "00000000" /f
 
+echo.
+
 echo Disabling Cortana...
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /V "AllowCortana" /t REG_DWORD /d "0" /f
+
+echo.
 
 echo Show File Extensions...
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideFileExt" /t REG_DWORD /d "0" /f
 
+echo.
+
 echo Disabling Immersive Context Menu...
 reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+
+echo.
 
 echo Disabling Taksbar Widgets...
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Dsh" /v "AllowNewsAndInterests" /t REG_DWORD /d "0" /f
