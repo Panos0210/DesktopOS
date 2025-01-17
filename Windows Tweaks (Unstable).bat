0b2a738d1894e8dda9a723a397c91f4b778d0712
panso <121546982+Panos0210@users.noreply.github.com>
2025-01-17 19:28:23 +0200

    Update and rename test.bat to Windows Tweaks (Unstable).bat

--- Windows Tweaks (Unstable).bat
+++ Windows Tweaks (Unstable).bat
@@ -22,7 +22,7 @@ echo 3) Disable Updates to Only 24H2
 echo 4) Remove Microsoft Edge
 echo 5) Exit
 
-choice /C 12345 /N /M "Select an option:"
+choice /C 12345 /N /M ">"
 
 if errorlevel 5 goto :exit
 if errorlevel 4 goto :remove_edge
@@ -52,9 +52,16 @@ goto :menu
 exit
 
 :banner
+
 echo.
 echo.
 echo                               ,,                    ,,                                                                                                    
 echo         `7MMF'     A     `7MF'db                  `7MM                                       MMP""MM""YMM                               `7MM              
 echo           `MA     ,MA     ,V                        MM                                       P'   MM   `7                                 MM              
-echo
+echo            VM:   ,VVM:   ,V `7MM  `7MMpMMMb.   ,M""bMM  ,pW"Wq.`7M'    ,A    `MF',pP"Ybd          MM `7M'    ,A    `MF'.gP"Ya   ,6"Yb.    MM  ,MP',pP"Ybd 
+echo             MM.  M' MM.  M'   MM    MM    MM ,AP    MM 6W'   `Wb VA   ,VAA   ,V  8I   `"          MM   VA   ,VAA   ,V ,M'   Yb 8)   MM    MM ;Y   8I   `" 
+echo             `MM A'  `MM A'    MM    MM    MM 8MI    MM 8M     M8  VA ,V  VA ,V   `YMMMa.          MM    VA ,V  VA ,V  8M""""""  ,pm9MM    MM;Mm   `YMMMa. 
+echo              :MM;    :MM;     MM    MM    MM `Mb    MM YA.   ,A9   VVV    VVV    L.   I8          MM     VVV    VVV   YM.    , 8M   MM    MM `Mb. L.   I8 
+echo               VF      VF    .JMML..JMML  JMML.`Wbmd"MML.`Ybmd9'     W      W     M9mmmP'        .JMML.    W      W     `Mbmmd' `Moo9^Yo..JMML. YA.M9mmmP' 
+echo.
+echo.
