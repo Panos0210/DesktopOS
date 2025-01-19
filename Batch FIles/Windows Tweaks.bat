c5caf027725e09c48606bb02b99efc8dd139d77d
panso <121546982+Panos0210@users.noreply.github.com>
2025-01-19 13:50:02 +0200

    Update Windows Tweaks.bat

--- Batch FIles/Windows Tweaks.bat
+++ Batch FIles/Windows Tweaks.bat
@@ -19,10 +19,10 @@ echo 3) Disable Updates to Only 24H2
 echo 4) Remove Microsoft Edge
 echo 5) System Corruption Scan
 echo 6) Update All Apps (Requires WinGet)
-choice /C 0123456 /N /M ">"
+choice /C:1234560 /N ">"
 set _erl=%errorlevel%
 
-if %_erl%==0 exit /b
+if %_erl%==11 exit /b
 if %_erl%==6 setlocal & start "" powershell -NoProfile -ExecutionPolicy Bypass -Command "winget update --all"
 if %_erl%==5 setlocal & start "" powershell -NoProfile -ExecutionPolicy Bypass -Command "Write-Host 'Running Chkdsk (1/4)' -ForegroundColor Green; chkdsk /scan; Write-Host 'Running 1st SFC scan (2/4)' -ForegroundColor Green; sfc /scannow; Write-Host 'Running DISM (3/4)' -ForegroundColor Green; DISM /Online /Cleanup-Image /RestoreHealth; Write-Host 'Running 2nd SFC scan (4/4)' -ForegroundColor Green; sfc /scannow; pause"
 if %_erl%==4 setlocal & start "" powershell -NoProfile -ExecutionPolicy Bypass -Command "irm https://rebrand.ly/remove edge | iex"
