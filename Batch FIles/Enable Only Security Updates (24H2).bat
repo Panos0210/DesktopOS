d0a1784ce628817128d1e4cc215bebed380214fa
panso <121546982+Panos0210@users.noreply.github.com>
2025-05-10 12:41:06 +0300

    Delete Batch FIles/Enable Only Security Updates (24H2).bat

--- Batch FIles/Enable Only Security Updates (24H2).bat
+++
@@ -1,6 +0,0 @@
-@echo off 
-echo Enable Only Security Updates...
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "ProductVersion" /t REG_SZ /d "Windows 11" /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "TargetReleaseVersion" /t REG_DWORD /d "1" /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "TargetReleaseVersionInfo" /t REG_SZ /d "24H2" /f
-pause
