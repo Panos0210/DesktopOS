6b90e1ca5a127d608ff678fd7c02ef87e32d0c27
panso <121546982+Panos0210@users.noreply.github.com>
2025-05-31 23:59:04 +0300

    Update Windows Tweaks.bat

--- Batch FIles/Windows Tweaks.bat
+++ Batch FIles/Windows Tweaks.bat
@@ -25,7 +25,7 @@ choice /C:123456780 /N /M ">"
 set _erl=%errorlevel%
 
 if %_erl%==9 exit /b
-if %_erl%==8 setlocal & start "" powershell -NoProfile -ExecutionPolicy Bypass -Command "winget upgrade --all; pause"
+if %_erl%==8 setlocal & powershell -NoProfile -ExecutionPolicy Bypass -Command "winget upgrade --all; pause"
 if %_erl%==7 setlocal & start "" powershell -NoProfile -ExecutionPolicy Bypass -Command "Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe"
 if %_erl%==6 setlocal & start "" powershell -NoProfile -ExecutionPolicy Bypass -Command "Write-Host 'Running Chkdsk (1/4)' -ForegroundColor Green; chkdsk /scan; Write-Host 'Running 1st SFC scan (2/4)' -ForegroundColor Green; sfc /scannow; Write-Host 'Running DISM (3/4)' -ForegroundColor Green; DISM /Online /Cleanup-Image /RestoreHealth; Write-Host 'Running 2nd SFC scan (4/4)' -ForegroundColor Green; sfc /scannow; pause"
 if %_erl%==5 setlocal & start "" powershell -NoProfile -ExecutionPolicy Bypass -Command "irm https://rebrand.ly/removeedge | iex"
