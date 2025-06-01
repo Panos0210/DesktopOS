503e7efe55c0489a179502248337c5fd7cc3f373
panso <121546982+Panos0210@users.noreply.github.com>
2025-06-01 12:29:30 +0300

    Update Windows Tweaks.bat

--- Batch FIles/Windows Tweaks.bat
+++ Batch FIles/Windows Tweaks.bat
@@ -33,7 +33,7 @@ if %_erl%==7 setlocal & start "" powershell -NoProfile -ExecutionPolicy Bypass -
 if %_erl%==6 setlocal & start "" powershell -NoProfile -ExecutionPolicy Bypass -Command "Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe"
 if %_erl%==5 setlocal & start "" powershell -NoProfile -ExecutionPolicy Bypass -Command "Write-Host 'Running Chkdsk (1/4)' -ForegroundColor Green; chkdsk /scan; Write-Host 'Running 1st SFC scan (2/4)' -ForegroundColor Green; sfc /scannow; Write-Host 'Running DISM (3/4)' -ForegroundColor Green; DISM /Online /Cleanup-Image /RestoreHealth; Write-Host 'Running 2nd SFC scan (4/4)' -ForegroundColor Green; sfc /scannow; pause"
 if %_erl%==4 setlocal & start "" powershell -NoProfile -ExecutionPolicy Bypass -Command "irm https://rebrand.ly/removeedge | iex"
-if %_erl%==3 setlocal & goto :updateoptions
+if %_erl%==3 setlocal & cls; goto :updateoptions
 if %_erl%==2 setlocal & start "" powershell -NoProfile -ExecutionPolicy Bypass -Command "irm https://rebrand.ly/windebloat | iex"
 if %_erl%==1 setlocal & start "" powershell -NoProfile -ExecutionPolicy Bypass -Command "irm https://rebrand.ly/alltweaks | iex"
 
