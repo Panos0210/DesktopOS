8cb437349135e3aeb4a283309fb9ae6b2f4c6775
panso <121546982+Panos0210@users.noreply.github.com>
2025-12-22 11:42:59 +0000

    Create Privacy Tweaks.bat

---
+++ Batch FIles/Privacy Tweaks.bat
@@ -0,0 +1,54 @@
+@echo off
+title Privacy Tweaks
+
+echo Applying Privacy Tweaks...
+echo.
+
+echo Disabling Advertising ID...
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\AdvertisingInfo" /v "Enabled" /t REG_DWORD /d "0" /f
+echo.
+
+echo Disabling App Launch Tracking...
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Start_TrackProgs" /t REG_DWORD /d "0" /f
+echo.
+
+echo Disabling Website Access to Language List...
+reg add "HKCU\Control Panel\International\User Profile" /v "HttpAcceptLanguageOptOut" /t REG_DWORD /d "1" /f
+echo.
+
+echo Disabling Tailored Experiences...
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Privacy" /v "TailoredExperiencesWithDiagnosticDataEnabled" /t REG_DWORD /d "0" /f
+echo.
+
+echo Disabling Feedback Frequency...
+reg add "HKCU\Software\Microsoft\Siuf\Rules" /v "NumberOfSIUFInPeriod" /t REG_DWORD /d "0" /f
+echo.
+
+echo Disabling Clipboard History...
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "AllowClipboardHistory" /t REG_DWORD /d "0" /f
+echo.
+
+echo Disabling Cloud Clipboard Sync...
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "AllowCrossDeviceClipboard" /t REG_DWORD /d "0" /f
+echo.
+
+echo Disabling Input Personalization...
+reg add "HKCU\Software\Microsoft\InputPersonalization" /v "RestrictImplicitInkCollection" /t REG_DWORD /d "1" /f
+reg add "HKCU\Software\Microsoft\InputPersonalization" /v "RestrictImplicitTextCollection" /t REG_DWORD /d "1" /f
+reg add "HKCU\Software\Microsoft\InputPersonalization\TrainedDataStore" /v "HarvestContacts" /t REG_DWORD /d "0" /f
+echo.
+
+echo Disabling Online Speech Recognition...
+reg add "HKCU\Software\Microsoft\Speech_OneCore\Settings\OnlineSpeechPrivacy" /v "HasAccepted" /t REG_DWORD /d "0" /f
+echo.
+
+echo Disabling Camera on Lock Screen...
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Personalization" /v "NoLockScreenCamera" /t REG_DWORD /d "1" /f
+echo.
+
+echo.
+echo ============================================
+echo       Privacy Tweaks Applied!
+echo ============================================
+echo.
+pause
