f1c4f042cfdb7b13c8e8bdcb8f29482da58f9911
panso <121546982+Panos0210@users.noreply.github.com>
2025-12-22 11:17:59 +0000

    Update Gaming Optimisations.bat

--- Batch FIles/Gaming Optimisations.bat
+++ Batch FIles/Gaming Optimisations.bat
@@ -45,6 +45,8 @@ if %errorlevel% neq 0 (
     powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
 )
 for /f "tokens=4" %%a in ('powercfg -list ^| findstr /C:"Ultimate Performance"') do set "ultimatePlanGUID=%%a"
-powercfg -setactive %ultimatePlanGUID%echo.
+powercfg -setactive %ultimatePlanGUID%
+
+echo.
 
 pause
